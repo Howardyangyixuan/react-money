044454215e5a8789f7ae37ed8a01ffdc75cae3be
howardyangyixuan <howardyangyixuan@gmail.com>
2020-12-12 22:31:33 +0800

    使用styled-component制作一个Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
+const Button = styled.button`
+  color: red;
+`;
 function App() {
   return (
     <div className="App">
+      <Button>hi</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
