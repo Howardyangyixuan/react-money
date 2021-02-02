255201083c87c3573e8f2392a7e31c7c802cb29c
howardyangyixuan <howardyangyixuan@gmail.com>
2021-02-02 09:53:16 +0800

    使用react-router添加基础导航

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,66 @@
 import React from 'react';
+import {
+  // BrowserRouter as Router,
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-     初始化
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Redirect from="/" exact to="/tags"/>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
+}
+
+function NoMatch() {
+  return <h2>404</h2>;
+}
 export default App;
