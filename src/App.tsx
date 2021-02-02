ee27a5e39ba580fb9f8daca9cbb6979d00ff34ba
howardyangyixuan <howardyangyixuan@gmail.com>
2021-02-02 15:13:45 +0800

    封装layout，404页面看不到导航栏

--- src/App.tsx
+++ src/App.tsx
@@ -8,93 +8,53 @@ import {
 } from 'react-router-dom';
 import styled from 'styled-components';
 import Nav from './Components/Nav';
-
-const Wrapper = styled.div`
-height: 100vh;
-display: flex;
-flex-direction: column;
-`;
-
-const Main = styled.div`
-flex-grow: 1;
-//自己的区域加滚动条
-overflow: auto;
-`;
+import Layout from './Components/Layout';
 
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Redirect from="/" exact to="/tags"/>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
-        </Main>
-        <Nav/>
-      </Wrapper>
+      <Switch>
+        <Redirect from="/" exact to="/tags"/>
+        <Route path="/tags">
+          <Tags/>
+        </Route>
+        <Route path="/money">
+          <Money/>
+        </Route>
+        <Route path="/statistics">
+          <Statistics/>
+        </Route>
+        <Route path="*">
+          <NoMatch/>
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
 function Statistics() {
-  return <h2>Statistics</h2>;
+  return (
+    <Layout>
+      <h2>Statistics</h2>
+    </Layout>
+  );
 }
 
 function Tags() {
-  return <h2>Tags</h2>;
+  return (
+    <Layout>
+      <h2>Tags</h2>
+    </Layout>
+  );
 }
 
 function Money() {
   return (
-    <>
-      <h2>Money </h2>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-      <p>h</p>
-    </>
+    <Layout>
+      <h2>Money</h2>
+    </Layout>
   );
-
 }
 
 function NoMatch() {
