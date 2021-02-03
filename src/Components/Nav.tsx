7799f798f4ae6bff153cabb7607980a47cccbd2d
howardyangyixuan <howardyangyixuan@gmail.com>
2021-02-03 08:07:04 +0800

    优化Nav，使得整个区域点击均可跳转，同时点击后高亮

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,21 +1,27 @@
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 
 import React from 'react';
 import styled from 'styled-components';
-import Icon from "./Icon"
+import Icon from './Icon';
 
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgb(0,0,0,0.25);
   > ul{
     display: flex;
     > li{
+      a{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 16px;
+          &.selected{
+            color:red;
+            fill:red;
+          }
+        }
       text-align: center;
       width: 33.3%;
-      padding: 16px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      > svg{
+      svg{
       height: 36px;
       width: 36px;
       }
@@ -27,16 +33,23 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="tag"/>
-          <Link to="/tags"> 标签页</Link>
+          <NavLink to="/tags" activeClassName="selected">
+            <Icon name="tag"/>
+            标签页
+          </NavLink>
+
         </li>
         <li>
-          <Icon name="money"/>
-          <Link to="/money">记账页</Link>
+          <NavLink to="/money" activeClassName="selected">
+            <Icon name="money"/>
+            记账页
+          </NavLink>
         </li>
         <li>
-          <Icon name="statistics"/>
-          <Link to="/statistics">统计页</Link>
+          <NavLink to="/statistics" activeClassName="selected">
+            <Icon name="statistics"/>
+            统计页
+          </NavLink>
         </li>
       </ul>
     </NavWrapper>
