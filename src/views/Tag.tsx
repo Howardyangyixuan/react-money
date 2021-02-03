aceec99209fd6fd30d93fc68846f82e85b543142
howardyangyixuan <howardyangyixuan@gmail.com>
2021-02-03 08:56:52 +0800

    开始Tag页面，完成HTML结构

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,12 +1,57 @@
 import Layout from '../Components/Layout';
 import React from 'react';
+import styled from 'styled-components';
+
+const TagsSection = styled.section`
+`;
+const NotesSection = styled.section`
+`;
+const CategorySection = styled.section`
+`;
+const NumberPadSection = styled.section`
+`;
 
 function Tags() {
   return (
     <Layout>
+      <TagsSection>
+        <ol>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ol>
+        <button>新增标签</button>
+      </TagsSection>
+      <NotesSection>
+        <button>支出</button>
+        <button>收入</button>
+      </NotesSection>
+      <CategorySection>
+        <label>
+          <span>备注</span>
+          <input type="text" value="在这里添加备注"/>
+        </label>
+      </CategorySection>
+      <NumberPadSection>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>OK</button>
+        <button>0</button>
+        <button>.</button>
+      </NumberPadSection>
       <h2>Tags</h2>
     </Layout>
   );
 }
 
-export default Tags
+export default Tags;
