48a508b3f017140f3e51081d194c4293ce8069d7
howardyangyixuan <howardyangyixuan@gmail.com>
2021-02-08 17:48:30 +0800

    修复部署代码

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,7 @@ cd build &&
 git init &&
 git add . &&
 git commit -m 'deploy' &&
+git remote add origin git@github.com:Howardyangyixuan/react-morney-website.git &&
 git branch -M main &&
 git push -u origin main -f
 cd -
